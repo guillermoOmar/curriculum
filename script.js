7334dcc9f6640d137f88ec55f89aab5dac0f5ab3
gdecoppet <gdecoppet@gmail.com>
2023-09-27 14:24:11 -0300

    primer commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function mostrarContenido(sectionId) {
+  // Ocultar todas las secciones
+  var sections = document.getElementsByClassName('content-section');
+  for (var i = 0; i < sections.length; i++) {
+    sections[i].style.display = 'none';
+  }
+
+  // Mostrar la sección seleccionada
+  var selectedSection = document.getElementById(sectionId);
+  if (selectedSection) {
+    selectedSection.style.display = 'block';
+  }
+}
